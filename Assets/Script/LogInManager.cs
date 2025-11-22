c5b8b4fa7e309260940362527028eedac0100691
duywingchun <crysgarena2006@gmail.com>
2025-11-22 09:33:18 +0700

    Initial commit without Library/.vs

---
+++ Assets/Script/LogInManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;  // Nếu dùng TextMeshPro
+using UnityEngine.UI;
+
+public class LoginUI : MonoBehaviour
+{
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+    public Button loginButton;
+    public TcpClientManager tcpClientManager;
+
+    void Start()
+    {
+        // Gắn sự kiện cho nút login
+        loginButton.onClick.AddListener(OnLoginClicked);
+    }
+
+    void OnLoginClicked()
+    {
+        string email = emailInput.text;
+        string password = passwordInput.text;
+
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            Debug.LogWarning("Vui lòng nhập đầy đủ email và mật khẩu!");
+            return;
+        }
+
+        tcpClientManager.LoginAccount(email, password);
+    }
+}
