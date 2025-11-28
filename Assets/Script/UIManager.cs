239954b3f1f7d29c9b2a78aed64a6dba667adc34
Duynguyennc <nguyenduykhtn@gmail.com>
2025-11-28 15:36:22 +0700

    Build map

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject loginPanel;
+    public GameObject registerPanel;
+
+    void Start()
+    {
+        // Khi bắt đầu chỉ hiển thị form đăng nhập
+        loginPanel.SetActive(true);
+        registerPanel.SetActive(false);
+    }
+
+    public void ShowRegisterPanel()
+    {
+        loginPanel.SetActive(false);
+        registerPanel.SetActive(true);
+    }
+
+    public void ShowLoginPanel()
+    {
+        loginPanel.SetActive(true);
+        registerPanel.SetActive(false);
+    }
+}
